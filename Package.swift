a09b1ce8e8d1ef703e63027e13af98e036a4d557
Ydna <0xEFEFEF@gmail.com>
2024-06-02 02:44:12 +0800

    refactor: rename package name to avoid conflict with apple sdk

--- Package.swift
+++ Package.swift
@@ -2,48 +2,48 @@
 import PackageDescription
 
 let package = Package(
-    name: "SQLite.swift",
-    platforms: [
-        .iOS(.v11),
-        .macOS(.v10_13),
-        .watchOS(.v4),
-        .tvOS(.v11),
-        .visionOS(.v1)
-    ],
-    products: [
-        .library(
-            name: "SQLite",
-            targets: ["SQLite"]
-        )
-    ],
-    targets: [
-        .target(
-            name: "SQLite",
-            exclude: [
-                "Info.plist"
-            ]
-        ),
-        .testTarget(
-            name: "SQLiteTests",
-            dependencies: [
-                "SQLite"
-            ],
-            path: "Tests/SQLiteTests",
-            exclude: [
-                "Info.plist"
-            ],
-            resources: [
-                .copy("Resources")
-            ]
-        )
-    ]
+	name: "SQLiteSwift",
+	platforms: [
+		.iOS(.v11),
+		.macOS(.v10_13),
+		.watchOS(.v4),
+		.tvOS(.v11),
+		.visionOS(.v1),
+	],
+	products: [
+		.library(
+			name: "SQLiteSwift",
+			targets: ["SQLiteSwift"]
+		),
+	],
+	targets: [
+		.target(
+			name: "SQLiteSwift",
+			exclude: [
+				"Info.plist",
+			]
+		),
+		.testTarget(
+			name: "SQLiteSwiftTests",
+			dependencies: [
+				"SQLiteSwift",
+			],
+			path: "Tests/SQLiteTests",
+			exclude: [
+				"Info.plist",
+			],
+			resources: [
+				.copy("Resources"),
+			]
+		),
+	]
 )
 
 #if os(Linux)
 package.dependencies = [
-    .package(url: "https://github.com/stephencelis/CSQLite.git", from: "0.0.3")
+	.package(url: "https://github.com/stephencelis/CSQLite.git", from: "0.0.3"),
 ]
 package.targets.first?.dependencies += [
-    .product(name: "CSQLite", package: "CSQLite")
+	.product(name: "CSQLite", package: "CSQLite"),
 ]
 #endif
